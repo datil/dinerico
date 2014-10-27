338d514c88e5da867ecee7a94937475e1fba5e80
Alejandro Vinueza <alejandrovinuezap@gmail.com>
2014-10-27 14:21:36 -0500

    Nuevo banner y header dinerico

--- web/app/scripts/main.js
+++ web/app/scripts/main.js
@@ -1,1 +1,15 @@
-console.log('\'Allo \'Allo!');
+var t = $('#content').offset().top - 80;
+
+$(document).scroll(function(){
+	'use strict';
+	    if($(this).scrollTop() > t)
+	    {   
+	    		$('.nav-dinerico').css({'background-color':'#20d3a0'});
+	    		$('.nav-dinerico .container').css({'border-bottom':'none'});
+
+	    }
+	    else{
+	    	$('.nav-dinerico').css({'background-color':'transparent'});
+	    	$('.nav-dinerico .container').css({'border-bottom':'thin solid #fff'});
+	    } 
+});
